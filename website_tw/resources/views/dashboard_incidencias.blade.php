f4eb3e1f6a3ac3aaba07072e7f8280bbe7127392
Olga <cchengcheng2004@gmail.com>
2026-05-21 14:59:14 +0200

    selector

--- website_tw/resources/views/dashboard_incidencias.blade.php
+++ website_tw/resources/views/dashboard_incidencias.blade.php
@@ -4,19 +4,17 @@
     <div class="container my-4">
         <h1 class="mb-4">Incidencias del municipio</h1>
         
-        <form action="{{ route('lista_incidencias') }}" method="GET" class="mb-4">
+        <form action="{{ route('lista_incidencias') }}" method="GET" class="d-flex justify-content-start mb-4 mx-2">
             <div class="row align-items-end">
-                <div class="col-12 col-md-4">
-                    <label for="estado" class="form-label fw-bold">Filtro: </label>
-                    <select name="estado" id="estado" class="form-select" onchange="this.form.submit()">
-                        <option value="">Cualquier estado</option>
-                        <option value="pendiente" {{ request('estado') == 'pendiente' ? 'selected' : '' }}>Pendiente</option>
-                        <option value="en_proceso" {{ request('estado') == 'en_proceso' ? 'selected' : '' }}>En proceso</option>
-                        <option value="solucionado" {{ request('estado') == 'solucionado' ? 'selected' : '' }}>Solucionado</option>
-                    </select>
-                </div>
+                <select name="estado" id="estado" class="form-select form-select-sm rounded-pill border-primary" style="width: 200px;" onchange="this.form.submit()">
+                    <option value="">Cualquier estado</option>
+                    <option value="pendiente" {{ request('estado') == 'pendiente' ? 'selected' : '' }}>Pendiente</option>
+                    <option value="en_proceso" {{ request('estado') == 'en_proceso' ? 'selected' : '' }}>En proceso</option>
+                    <option value="solucionado" {{ request('estado') == 'solucionado' ? 'selected' : '' }}>Solucionado</option>
+                </select>
             </div>
         </form>
+        
 
         <div class="row g-4">
 
